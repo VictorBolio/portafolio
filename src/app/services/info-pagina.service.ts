b6e7e2e4614b85ec67083fc27faf34a94f90b645
Victor Bolio <insticmx@gmail.com>
2019-01-28 16:13:00 -0600

    Fin de la Seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,37 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-    //console.log('Servicio de infoPagina listo.');
+    this.cargarInfo();
+    this.cargarEquipo();
 
-        // Leer archivo Json
-        this.http.get('assets/data/data-pagina.json')
-          .subscribe((resp: InfoPagina) => {
+  }
 
-            this.cargada = true;
-            this.info = resp;
-            console.log(resp);
+  private cargarInfo() {
 
-          });
+    // Leer archivo Json
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina) => {
+
+      this.cargada = true;
+      this.info = resp;
+
+    });
+
+  }
+
+  private cargarEquipo() {
+
+      // Leer archivo Json
+      this.http.get('https://angular-html-e08ab.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+  
+        this.equipo = resp;
+        console.log(resp);
+  
+      });
   }
 }
